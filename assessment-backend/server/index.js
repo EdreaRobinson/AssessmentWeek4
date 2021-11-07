bf031971f4091b847264e7db93ba71ff1301df20
Edrea Robinson <edrea1979@gmail.com>
2021-11-07 00:02:59 -0500

    Setup and New Feature completed

--- assessment-backend/server/index.js
+++ assessment-backend/server/index.js
@@ -22,4 +22,16 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+app.get("/api/fortune", (req, res) => {
+  const fortune = ["A gambler not only will lose what he has, but also will lose what he doesn’t have.", "Advice is like kissing. It costs nothing and is a pleasant thing to do.", "An acquaintance of the past will affect you in the near future.", "Each day, compel yourself to do something you would rather not do.", "Imagination rules the world."
+  ];
+
+  // choose random fortune
+  let randomIndex2 = Math.floor(Math.random() * fortune.length);
+  let randomFortune = fortune[randomIndex2];
+
+  res.status(200).send(randomFortune);
+  
+});
+
 app.listen(4000, () => console.log("Server running on 4000"));
